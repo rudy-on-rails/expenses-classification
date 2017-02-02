352dc30b1c7be7d30f454c92c4e9301c28af72b0
Rudy Seidinger <rudyseidinger@gmail.com>
2017-02-02 11:10:10 +0000

    Start implementing the consoleapp

--- src/org/rudyseidinger/expensesclassification/consoleapp/ConsoleApp.java
+++ src/org/rudyseidinger/expensesclassification/consoleapp/ConsoleApp.java
@@ -1,5 +1,6 @@
 package org.rudyseidinger.expensesclassification.consoleapp;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.rudyseidinger.expensesclassification.csv.CsvFileReader;
@@ -14,7 +15,17 @@ public class ConsoleApp {
 			String filePath = args[0];
 			System.out.println("Loading: " + filePath + " ...");
 			ArrayList<ArrayList<String>> data = new CsvFileReader(filePath).readCsvContents();
-			System.out.println(data);
+			System.out.println("The first row is: " + data.get(0));
+			System.out.println("Enter the column names to match where data types are (index-based):");
+			System.out.println("Title column:");
+			try {
+				int titleIdx = System.in.read();
+				System.out.println("Value column:");
+				int valueColumn = System.in.read();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.out.println(e.getMessage());
+			}			
 		}	
 	}
 

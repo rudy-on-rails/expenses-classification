3bcc13ba455afec1703b168388ef708ecfcca9b9
Rudy Seidinger <rudyseidinger@gmail.com>
2017-02-09 15:29:38 +0000

    Apply some DRY

--- src/org/rudyseidinger/expensesclassification/consoleapp/ConsoleApp.java
+++ src/org/rudyseidinger/expensesclassification/consoleapp/ConsoleApp.java
@@ -14,45 +14,49 @@ public class ConsoleApp {
 	
 	public static void main(String[] args) {	
 		if (args.length == 0){
-			System.out.println("You need to supply the path to a file before continuing...");
+			log("You need to supply the path to a file before continuing...");
 		}
 		else{
 			ArrayList<ArrayList<String>> data = readCsvFile(args[0]);			
 			readColumnInputs();			
 			readAndClassifyExpenses(data);
-		}	
+		}
 	}
 
 	private static ArrayList<ArrayList<String>> readCsvFile(String filePath) { 
-		System.out.println("Loading: " + filePath + " ...");
+		log("Loading: " + filePath + " ...");
 		ArrayList<ArrayList<String>> data = new CsvFileReader(filePath).readCsvContents();
-		System.out.println("The first row is: " + data.get(0));
+		log("The first row is: " + data.get(0));
 		return data;
 	}
 
 	private static void readAndClassifyExpenses(ArrayList<ArrayList<String>> data) {
 		ArrayList<Expense> expensesList = new ExpensesParser(data).parseExpenses(titleIdx, expenseAmountIdx, dateIdx);
-		System.out.println("Classifying expenses...");
+		log("Classifying expenses...");
 		new ClassifyExpenses(expensesList).Execute();
-		System.out.println("Expenses classifed");
+		log("Expenses classifed");
 	}
 
 	private static void readColumnInputs() {
-		System.out.println("Enter the column names to match where data types are (index-based):");
+		log("Enter the column names to match where data types are (index-based):");
 		Scanner scanner = new Scanner(System.in);
 		
 		try {
-			System.out.println("Title column index:");
+			log("Title column index:");
 			titleIdx = scanner.nextInt();
-			System.out.println("Amount column index:");
+			log("Amount column index:");
 			expenseAmountIdx = scanner.nextInt();
-			System.out.println("Date of expense index:");
+			log("Date of expense index:");
 			dateIdx = scanner.nextInt();	
-			scanner.close();		
-			
+			scanner.close();					
 		} catch (InputMismatchException e) {
-			System.out.println("Enter valid values...");
+			log("Enter valid values...");
 		}
 	}
+	
+	private static void log(String text){
+		System.out.println(text);
+	}
+	
 
 }
